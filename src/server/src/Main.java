8c50ed8b2ef854defead0c3da504362b3f8f2e64
tinou974 <etiennepaita@gmail.com>
2022-01-05 21:12:05 +0100

    encodage d'images

--- src/server/src/Main.java
+++ src/server/src/Main.java
@@ -1,10 +1,29 @@
 package server.src;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import client.src.repository.Catalogue;
+import client.src.repository.Categorie;
+import client.src.repository.Image;
 
 public class Main {
     public static void main(String[] args) {
         Catalogue c =new Catalogue();
+        c.showCatalogue();
+        List<Categorie> l = c.getCatalogue();
+        Image i = l.get(0).getCategoryImages().get(0);
+        File f = new File("src/server/img/new.png");
+        if(!f.exists())
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        c.decoder(i.getImg(), "src/server/img/new.png");
     }
     
 }
