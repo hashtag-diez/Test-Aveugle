87b48f5d22d55ed0d28f16c638592813091719de
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-01-06 17:49:22 +0100

    Add code to schedule the timer starting

--- src/client/src/test/Timer.java
+++ src/client/src/test/Timer.java
@@ -1,8 +1,10 @@
 package client.src.test;
 
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Timer {
@@ -10,28 +12,29 @@ public class Timer {
   public volatile boolean ongoing = true;
   public static void main(String[] args) throws InterruptedException {
     Timer timer = new Timer();
-    ExecutorService pool = Executors.newCachedThreadPool();
-    pool.submit(() -> {
-      System.out.println("TIMER");
-      while(timer.nbQuestions>0){
-        long debut=System.currentTimeMillis();
+    Instant server = Instant.now().plus(10, ChronoUnit.SECONDS);
+    System.out.println("Envoi de la requete SCORE_REFRESH");
+    TimeUnit.SECONDS.sleep(8);
+    System.out.println("Réception de la réponse");
+    Instant client = Instant.now();
+    Duration d = Duration.between(client, server);
+    ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+    Runnable countdown = () -> {
+      System.out.println("DEBUT TIMER");   
+      long debut=System.currentTimeMillis();
         while(timer.ongoing){
           try {
             TimeUnit.SECONDS.sleep(1);
           } catch (InterruptedException e) {}
           long enCours=System.currentTimeMillis()-debut;
           long secondes=enCours/1000;
-          System.out.println(secondes + "s sont passées");
+          if(secondes == 10){timer.ongoing = false;}
+          System.out.println(secondes + "s");
         }
-        timer.nbQuestions --;
-        System.out.println("TIMER RESET");;
-        timer.ongoing = true;
-      }
-    });
-    int i = 0;
-    while(i<=10){
-      TimeUnit.SECONDS.sleep(new Random().nextInt(10) + 1);
-      timer.ongoing = false;
-    }
+      System.out.println("FIN TIMER");;
+      timer.ongoing = true; 
+      ses.shutdown();
+    };
+    ses.schedule(countdown, d.toSeconds() , TimeUnit.SECONDS);
   }
 }
