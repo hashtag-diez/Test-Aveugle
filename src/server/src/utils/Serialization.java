52cf7701db1069b9afa9abdab2215590ecc08dd5
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-01-06 12:13:56 +0100

    Add code for more serialization methods

--- src/server/src/utils/Serialization.java
+++ src/server/src/utils/Serialization.java
@@ -6,33 +6,22 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 import server.src.model.Load;
 
 public class Serialization {
 
-  @SuppressWarnings("unchecked")
-  public static Load deserializeLoad(byte[] bytes) throws ClassNotFoundException{
-    try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes)) {
-      ObjectInput in = null;
-      try {
-      	in = new ObjectInputStream(bis);
-      	Map<String,String> params = (Map<String,String>) in.readObject(); 
-        Load res = new Load(params.get("type"), params);
-      	return res;
-      }  
-      finally{
-      	try {
-      		if (in != null) {
-      			in.close();
-      		}
-      	} catch (IOException e) {
-      		throw e;
-      	}
-      }
-    } catch (IOException e) {
+	@SuppressWarnings("unchecked")
+	public static Load deserializeLoad(byte[] bytes, boolean isRequest) throws ClassNotFoundException{
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);    
+		ObjectInput in = null;
+		try {
+			in = new ObjectInputStream(bis);
+			Map<String,Map<String,String>> data = (Map<String,Map<String,String>>) in.readObject(); 
+			Load res = new Load(data.get("header"), data , isRequest);
+			return res;
+		} catch (IOException e) {
       e.printStackTrace();
     }
     return null;
@@ -42,12 +31,33 @@ public class Serialization {
 		ObjectOutputStream out = null;
 		try{
 			out = new ObjectOutputStream(bos);
-      Map<String,Map<String,String>> results = new HashMap<String,Map<String,String>>();
-      Map<String,String> type = new HashMap<String,String>();
-      type.put("name", load.getType().toString());
-      results.put("type",type);
-      results.put("results", load.getData().get("results"));
+      Map<String,Map<String,String>> results = load.convertToMap();
 			out.writeObject(results);
+			out.flush();
+			byte[] res = bos.toByteArray();
+			return res;
+		}
+		catch(IOException e){throw e;}
+	}
+	@SuppressWarnings("unchecked")
+	public static Map<String,Map<String,String>> deserializeMap(byte[] bytes) throws ClassNotFoundException{
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);    
+		ObjectInput in = null;
+		try {
+			in = new ObjectInputStream(bis);
+			Map<String,Map<String,String>> map = (Map<String,Map<String,String>>) in.readObject(); 
+			return map;
+		} catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+	}
+	public static byte[] serializeMap(Map<String,Map<String,String>> map) throws IOException{
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream out = null;
+		try{
+			out = new ObjectOutputStream(bos);
+			out.writeObject(map);
 			out.flush();
 			byte[] res = bos.toByteArray();
 			return res;
