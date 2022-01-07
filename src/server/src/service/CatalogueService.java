b59d889d0c9c2b37eaee67f04ce46b8c09570f6d
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-01-07 11:31:34 +0100

    Add code to implement the run method for services

--- src/server/src/service/CatalogueService.java
+++ src/server/src/service/CatalogueService.java
@@ -1,7 +1,13 @@
 package server.src.service;
 
+import java.nio.channels.AsynchronousSocketChannel;
+
+import server.src.model.Load;
 import server.src.service.serviceinterface.ServiceInterface;
 
 public class CatalogueService implements ServiceInterface {
+  public void run(Load request, Load response, AsynchronousSocketChannel client){
+    
+  }
   //CHANNEL QUESTIONS
 }
--- src/server/src/service/OneGameService.java
+++ src/server/src/service/OneGameService.java
@@ -1,6 +1,15 @@
 package server.src.service;
 
+import java.nio.channels.AsynchronousSocketChannel;
+
+import server.src.model.Load;
+import server.src.service.serviceinterface.ServiceInterface;
+
 public class OneGameService implements ServiceInterface {
+
+  public void run(Load request, Load response, AsynchronousSocketChannel client){
+    
+  }
   /* 
     SCORE_REFRESH, //RESET des timers, INCREMENTE si réponse trouvée  
   // Prend en paramètre le nom du channel à démarrer 
