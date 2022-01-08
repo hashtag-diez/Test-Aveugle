06f97789180c9e8ee498ce1fbb0a32037a73ee55
Rashid <rashid.aliev@sp3d.co>
2022-01-08 19:30:39 +0100

    First view of UserConnection

--- src/client/src/network/UserConnection.java
+++ src/client/src/network/UserConnection.java
@@ -1,6 +1,10 @@
 package src.network;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import java.nio.channels.AsynchronousSocketChannel;
 import src.utils.Serialization;
@@ -10,7 +14,9 @@ import src.model.Type;
 import src.model.Status;
 import src.model.Channel;
 
-
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
 
 import java.nio.ByteBuffer;
 
@@ -22,66 +28,187 @@ public class UserConnection {
     this.line = line;
   }
 
-  public void waitForResponse(AsynchronousSocketChannel socket, String line)
+  public void waitForLoad(AsynchronousSocketChannel socket, String line)
       throws InterruptedException, ExecutionException, ClassNotFoundException, IOException {
     while (true) {
-      ByteBuffer buffer = ByteBuffer.allocate(1024);
+      ByteBuffer buffer = ByteBuffer.allocate(4024000);
       socket.read(buffer).get();
       handleResponse(buffer);
     }
   }
 
+  public Map<String, Map<String, String>> outputParser(String line) {
+    Map<String, Map<String, String>> request = new HashMap<String, Map<String, String>>();
+    Map<String, String> type = new HashMap<String, String>();
+    Map<String, String> params = new HashMap<String, String>();
+    String[] command = line.split(" ");
+    switch (command[0]) {
+      case "CHANNEL_CREATE":
+        System.out.println("On veut créer une room !");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        params.put("adminName", command[2]);
+        params.put("categorieName", command[3]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "CHANNEL_DELETE":
+        System.out.println("On veut quitter une room !");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        params.put("adminName", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "CHANNEL_START":
+        System.out.println("On veut commencer le jeu!");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "USER_CONNECT":
+        System.out.println("On veut se connecter à une room !");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        params.put("pseudo", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "USER_DISCONNECT":
+        System.out.println("On veut quitter à une room !");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        params.put("pseudo", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "USER_ANSWER":
+        System.out.println("On veut se connecter à une room !");
+        type.put("type", command[0]);
+        params.put("questionResponse", command[1]);
+        params.put("userAnswer", String.join(" ", Arrays.copyOfRange(command, 3, command.length)));
+        params.put("pseudo", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "CHANNEL_QUESTIONS":
+        System.out.println("On veut la liste des images pour la catégorie " + command[1]);
+        type.put("type", command[0]);
+        params.put("categorieName", command[1]);
+        params.put("channelName", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "GET_CHANNELS":
+        System.out.println("On veut la liste des rooms !");
+        type.put("type", command[0]);
+        request.put("header", type);
+        break;
+      case "SCORE_REFRESH":
+        System.out.println("On veut rafraichir le score !");
+        type.put("type", command[0]);
+        params.put("channelName", command[1]);
+        params.put("pseudo", command[2]);
+        request.put("header", type);
+        request.put("params", params);
+        break;
+      case "EXIT":
+        type.put("type", command[0]);
+        request.put("header", type);
+        break;
+      default:
+        break;
+    }
+    return request;
+  }
+
+  public void handleRequest(AsynchronousSocketChannel socket, String line, Scanner scanner)
+      throws InterruptedException, ExecutionException, IOException, ClassNotFoundException {
+    while (!line.equals("Bye !")) {
+      line = scanner.nextLine();
+      Map<String, Map<String, String>> request = outputParser(line);
+      socket.write(ByteBuffer.wrap(Serialization.serializeMap(request))).get();
+    }
+    sendDisconnectLoad(socket);
+    scanner.close();
+  }
+
   public void handleResponse(ByteBuffer buffer) throws ClassNotFoundException, IOException {
-    Load response = Serialization.deserializeMap(buffer.flip().array());
-    if (response.getStatus().equals(Status.OK)) {
-      switch (response.getType()) {
+
+    Load response = Serialization.deserializeLoad(buffer.flip().array());
+    if(response.getStatus().equals(Status.OK)){
+      switch(response.getType()){
         case CHANNEL_CREATE:
-          Channel newChannel = response.;
-          System.out.println("Création de " + newChannel.getName() + "(" + newChannel.getCuid() + ")");
-          Network.receiveChannel(newChannel);
+          Network.receiveGame(response.getData());
+          break;
+        case CHANNEL_DELETE:
+          Network.receiveDeconnection(response.getData())
+          break;
+        case CHANNEL_START:
+          break;
+        case USER_CONNECT:
+          break;
+        case USER_DISCONNECT:
+          break;
+        case USER_ANSWER:
+          break;
+        case CHANNEL_QUESTIONS:
           break;
         case GET_CHANNELS:
-          List<Channel> channels = (List<Channel>) response.getObj();
-          Network.updateChannelList(channels);
           break;
-        case GET_USERS:
-          List<User> users = (List<User>) response.getObj();
-          Network.updateUserList(users);
+        case SCORE_REFRESH:
           break;
-        case CREATE_MESSAGE:
-          Message message = (Message) response.getObj();
-          System.out.println(message.getUsername() + " " + message.getMessage() + " " + message.getCuid());
-          Network.receiveMessage(message);
-          break;
-        case GET_MESSAGES:er, avec juste la connexion au socket à gérer en plus
-          List<Message> messages = (List<Message>) response.getObj();
-          Network.updateMessageList(messages);
-          break;
-        case SIGNIN:
-        case SIGNUP:
-          User user = (User) response.getObj();
-          System.out.println("Connexion de " + user.getUsername());
-          if ((Network.isSigninRequestInProcess() || Network.isSignupRequestInProcess()))
-            Network.setCurrentUser(user);
-          else
-            Network.addUser(user);
-          break;
-        case SIGNOUT:
-          User disconnectedUser = (User) response.getObj();
-          System.out.println("Déconnexion de " + disconnectedUser.getUsername());
-          Network.setSignout(disconnectedUser);
-          break;
-        case DELETE_CHANNEL:
-          int cuid = (int) response.getObj();
-          System.out.println("Suppression de " + cuid);
-          Network.receiveDeleteChannel(cuid);
+        case EXIT:
           break;
         default:
           break;
-      }
-    } else {
+      } 
+    }else{
       System.out.println("Erreur");
-      Network.setAllRequestError();
+      //Network.setAllRequestError ??
+    }
+  }
+
+  public void sendDisconnectLoad(AsynchronousSocketChannel socket)
+      throws InterruptedException, ExecutionException, IOException {
+    Map<String, Map<String, String>> exit = outputParser("EXIT");
+    socket.write(ByteBuffer.wrap(Serialization.serializeMap(exit))).get();
+  }
+
+  public void setShutdownHook(AsynchronousSocketChannel socket) {
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      public void run() {
+        try {
+          Thread.sleep(200);
+          sendDisconnectLoad(socket);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          e.printStackTrace();
+        } catch (ExecutionException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  public void run() throws InterruptedException, ExecutionException, ClassNotFoundException, IOException {
+    try (AsynchronousSocketChannel sock = AsynchronousSocketChannel.open()) {
+      socket = sock;
+      socket.connect(new InetSocketAddress("localhost", 1237)).get();
+      Scanner scanner = new Scanner(System.in);
+      ExecutorService pool = Executors.newCachedThreadPool();
+      pool.submit(() -> {
+        try {
+          waitForLoad(socket, line);
+        } catch (ClassNotFoundException | InterruptedException | ExecutionException | IOException e) {
+          e.printStackTrace();
+        }
+      });
+      setShutdownHook(socket);
+      handleRequest(socket, line, scanner);
     }
   }
 
